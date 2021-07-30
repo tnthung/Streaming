f1386e7b02134d436dd95a2b561674f5dff24eb2
TNThung <hung11112002@gmail.com>
2021-07-30 15:41:32 +0800

    test msg

--- tmp.py
+++ tmp.py
@@ -1,4 +1,3 @@
-from math import trunc
 import os
 import cv2
 import time
@@ -10,7 +9,7 @@ from ffpyplayer.player import MediaPlayer
 def __newBufferVideo(file):
     tmp = MediaPlayer(file)
     tmp.set_pause(True)
-    return [cv2.VideoCapture(file), tmp]
+    return [cv2.VideoCapture(file), tmp, file]
 
 
 def __playSingleVideo(file):
@@ -31,22 +30,28 @@ def __playSingleVideo(file):
             img, t = audio
             time.sleep(val)
 
-    
-def __fetchBuffer(queue, buffer):
-    while True:
-        if len(buffer) < 3 and not queue:
-            buffer.append(__newBufferVideo(queue[0]))
+    buf[0].release()
+    cv2.destroyAllWindows()
 
-        time.sleep(1)
+
+def __fetchBuffer(queue, eof, buffer):
+    while not eof or queue:
+        if queue and len(buffer) < 2: buffer.append(__newBufferVideo(queue.pop(0)))
+        time.sleep(0.5)
 
 
 def player(file, eof, queue):
     buffer = []
-
-    threading.Thread(target=__fetchBuffer, args=[queue, buffer])
+    fetchingThread = threading.Thread(target=__fetchBuffer, args=[queue, eof, buffer])
+    fetchingThread.start()
 
     while True:
+        if (not eof or queue) and not buffer:
+            time.sleep(0.1)
+            continue
+
         buf = buffer.pop(0)
+        buf[1].set_pause(False)
 
         while True:
             grabbed, frame = buf[0].read()
@@ -63,16 +68,45 @@ def player(file, eof, queue):
                 img, t = audio
                 time.sleep(val)
 
-        queue.pop(0)
+        # os.remove(buf[2]) # remove the played file from clinet
+    
+        buf[0].release()
+        if not queue and eof and not buffer: break
 
-        if len(queue) > 1: buffer.append(__newBufferVideo(queue[0]))
-        elif eof: break
+    fetchingThread.join()
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
     # __playSingleVideo('5.mp4')
 
-    t = threading.Thread(target=player, args=["testVideo", [1], [f"{i}.mp4" for i in range(1, 14)]])
+    from random import randint
+
+    eof = []
+    queue = []
+
+    t = threading.Thread(target=player, args=["testVideo", eof, queue])
     t.start()
 
+    def tmp(queue, eof):
+        while not eof:
+            print(queue)
+            time.sleep(0.5)
+
+    p = threading.Thread(target=tmp, args=[queue, eof])
+    p.start()
+
+    i = iter([i for i in range(1, 14)])
+    while 1:
+        if len(queue) < 3: 
+            tmp = next(i)
+            queue.append(f"{tmp}.mp4")
+            if tmp == 13: break
+
+        time.sleep(randint(1, 5))
+
+    eof.append(1)
+
     t.join()
+    p.join()
+
