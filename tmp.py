09bb421601be935a5800f5cb4f6e6c3f0f78f3a1
TNThung <hung11112002@gmail.com>
2021-07-30 14:13:56 +0800

    played continuous with bug

--- tmp.py
+++ tmp.py
@@ -1,3 +1,4 @@
+from math import trunc
 import os
 import cv2
 import time
@@ -15,11 +16,9 @@ def __playSingleVideo(file):
 
     while True:
         grabbed, frame = buf[0].read()
-        audio, val = buf[1].get_frame()
+        audio  , val   = buf[1].get_frame()
 
-        if not grabbed:
-            break
-
+        if not grabbed: break
         if cv2.waitKey(1) & 0xFF == ord("q"): break
 
         cv2.imshow(file, frame)
@@ -31,13 +30,39 @@ def __playSingleVideo(file):
             time.sleep(val)
 
 
-def player(queue):
-    buf1 = __newBufferVideo(queue[0])
-    buf2 = __newBufferVideo(queue[1])
+def player(file, eof, queue):
+    buffer = []
+
+    buffer.append(__newBufferVideo(queue[0]))
 
     while True:
-        pass
+        buf = buffer.pop(0)
+
+        while True:
+            grabbed, frame = buf[0].read()
+            audio  , val   = buf[1].get_frame()
+
+            if not grabbed: break
+            if cv2.waitKey(1) & 0xFF == ord("q"): break
+
+            cv2.imshow(file, frame)
+
+            if val == "eof": break
+            elif audio is None: time.sleep(0.01)
+            else:
+                img, t = audio
+                time.sleep(val)
+
+        queue.pop(0)
+
+        if len(queue) > 1: buffer.append(__newBufferVideo(queue[0]))
+        elif eof: break
 
 
 if __name__ == '__main__':
-    __playSingleVideo('5.mp4')
+    # __playSingleVideo('5.mp4')
+
+    t = threading.Thread(target=player, args=["testVideo", [1], [f"{i}.mp4" for i in range(1, 14)]])
+    t.start()
+
+    t.join()
